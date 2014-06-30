13140d46e07628774bc39b3b1e2a8e68bcfc1854
Philweb Designer <philewbdesigner@Vons-Mac-mini.local>
2014-06-30 17:32:58 +0800

    update spreedly and trade

--- spreedly-master/spreedly.js
+++ spreedly-master/spreedly.js
@@ -23,6 +23,7 @@ Spreedly.createCORSRequest = function(method, url) {
 };
 
 Spreedly.tokenize = function(opt) {
+
 	
 	opt.firstName = Spreedly.escapeXml(opt.firstName);
 	opt.lastName = Spreedly.escapeXml(opt.lastName);
@@ -52,45 +53,50 @@ Spreedly.tokenize = function(opt) {
 	            <number>' + opt.number + '</number> \
 	            <month>' + opt.month + '</month> \
 	            <year>' + opt.year + '</year> \
+	            <address1>'+opt.address_1+'</address1> \
+	            <address2>'+opt.address_2+'</address2> \
+	            <city>'+opt.city+'</city> \
+	            <state>'+opt.state+'</state> \
+	            <zip>'+opt.zip+'</zip> \
+	            <country>'+opt.country+'</country> \
+	            <phone_number>'+opt.phone_number+'</phone_number> \
 	        </credit_card> \
+	        <email>'+opt.email+'</email> \
 	    </payment_method>'
 	);
- 
 };
 
 
-Spreedly.process = function(process) {
+// Spreedly.process = function(process) {
 	
-	process.firstName = Spreedly.escapeXml(process.firstName);
-	process.lastName = Spreedly.escapeXml(process.lastName);
-	process.number = ('' + process.number).replace(/\D/g, '');
+// 	process.processToken = Spreedly.escapeXml(process.processToken);
+// 	process.processAmount = Spreedly.escapeXml(process.processAmount);
+// 	process.currencyCode = Spreedly.escapeXml(process.currencyCode);
 	
-	var url = 'https://core.spreedly.com/v1/payment_methods.xml?environment_key=' + process.environmentKey;
-	var xhr = Spreedly.createCORSRequest('POST', url);
+// 	// var url = 'https://core.spreedly.com/v1/payment_methods.xml?environment_key=' + process.environmentKey;
+// 	// var url = 'https://core.spreedly.com/v1/gateways/'+process.processToken+'/purchase.xml?environment_key=' + process.environmentKey;
+// 	var url = 'https://core.spreedly.com/v1/gateways/CUK6uXH1GiQnxV2kW6apTPbJoi/purchase.xml';
+// 	var xhr = Spreedly.createCORSRequest('POST', url);
 	
-	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	xhr.setRequestHeader('Content-Type', 'application/xml');
+// 	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+// 	xhr.setRequestHeader('Content-Type', 'application/xml');
 	
-	xhr.onload = function() {
-	  var text = xhr.responseText;
-	  var token = $(text).find("payment_method").find("token").text();
-	  process.callback(token);
-	};
+// 	xhr.onload = function() {
+// 	  var text = xhr.responseText;
+// 	  var process_token = $(text).find("transaction").find("payment_method_token").text();
+// 	  process.callback(process_token);
+// 	};
 	
-	xhr.onerror = process.error || function() {
-	  alert('There was an error making the request.');
-	};
+// 	xhr.onerror = process.error || function() {
+// 	  alert('There was an error making the request.');
+// 	};
 	
-	xhr.send(
-	    '<payment_method> \
-	        <credit_card> \
-	            <first_name>' + process.firstName + '</first_name> \
-	            <last_name>' + process.lastName + '</last_name> \
-	            <number>' + process.number + '</number> \
-	            <month>' + process.month + '</month> \
-	            <year>' + process.year + '</year> \
-	        </credit_card> \
-	    </payment_method>'
-	);
+// 	xhr.send(
+// 	    '<transaction> \
+// 	        <payment_method_token>'+process.processToken+'</payment_method_token> \
+// 	        <amount>'+process.processAmount+'</amount> \
+// 	        <currency_code>'+process.currencyCode+'</currency_code> \
+// 	    </transaction>'
+// 	);
  
-};
+// };
